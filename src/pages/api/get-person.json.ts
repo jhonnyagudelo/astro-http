8c16448455273d49141f036a8d903d2c0a3d8f76
Jhonny Stiven Agudelo Tenorio <jhonny.agudelo3120@gmail.com>
2024-09-06 17:48:07 -0500

    first commit

---
+++ src/pages/api/get-person.json.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const person = {
+    name: 'Jhonny stiven agudelo',
+    age: '38 años'
+  }
+  return new Response(JSON.stringify(person), {
+    status: 201,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
+
+
+
