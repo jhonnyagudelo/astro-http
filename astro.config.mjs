bde2b71c785c504c661b673c3434c4e8702e29b1
Jhonny Stiven Agudelo Tenorio <jhonny.agudelo3120@gmail.com>
2024-09-21 10:19:30 -0500

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,21 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  output: 'hybrid',
 
-  adapter: node({
-    mode: 'standalone',
-  }),
+  // output: 'hybrid',
+  //
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
+  integrations: [mdx(), sitemap()],
+
+  output: 'server',
+  adapter: cloudflare(),
 });
